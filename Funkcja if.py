4e55473131b041c8d6bd71b549d53ddc4d5f7344
ReferTV <61411400+ReferTV@users.noreply.github.com>
2021-10-18 17:24:20 +0200

    Funkcja if

---
+++ Funkcja if.py
@@ -0,0 +1,11 @@
+x = input()
+x = int(x)
+
+# == wskazuje czy lewa strona jest równa prawej,
+# a != to znak nierówności, większe lub równe określamy
+# za pomocą >= lub <=
+
+if x == 5:
+    print("Tak!")
+else:
+    print("Nie!")
